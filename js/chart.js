150b1ad6bde7e45438a42565c3f03b57cf96fd58
pkp <pankajkumar.01869@gmail.com>
2017-07-23 17:41:35 +0530

    bj

---
+++ js/chart.js
@@ -0,0 +1,25 @@
+Highcharts.chart('container', {
+
+    title: {
+        text: 'Last 10 days Earnings'
+    },
+
+    xAxis: {
+        tickInterval: 1
+    },
+
+    yAxis: {
+        type: 'logarithmic',
+        minorTickInterval: 0.1
+    },
+
+    tooltip: {
+        headerFormat: '<b>{series.name}</b><br />',
+        pointFormat: 'x = {point.x}, y = {point.y}'
+    },
+
+    series: [{
+        data: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512],
+        pointStart: 1
+    }]
+});
